512221b27470a6ee3435fca82085280780fe50ec
Michael Buesch <mb@bu3sch.de>
2009-11-20 17:10:23 +0100

    Fix avg_power.sh
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- avg_power.sh
+++ avg_power.sh
@@ -3,11 +3,11 @@
 # Script to measure the average power drawn by
 # the device.
 #
-# Copyright (C) 2007 Michael Buesch <mb@bu3sch.de>
+# Copyright (C) 2007-2009 Michael Buesch <mb@bu3sch.de>
 # GPLv2+
 #
 
-PCIBX="./pcibx $@"	# path to pcibx
+PCIBX="./pcibx -V1 $@"	# path to pcibx
 cnt=70			# number of measurements
 
 
@@ -27,7 +27,7 @@ function measure # $1=what
 	RES="$(echo $tmp | cut -d' ' -f2)"
 }
 
-S="scale=6;"
+S="scale=3;"
 avg_a5="0"
 avg_a12="0"
 avg_a33="0"
@@ -49,10 +49,13 @@ avg_a33=$(echo "$S $avg_a33 / $cnt" | bc)
 va5=$(echo "$S 5 * $avg_a5" | bc)
 va12=$(echo "$S 12 * $avg_a12" | bc)
 va33=$(echo "$S 3.3 * $avg_a33" | bc)
+va_all=$(echo "$S $va5 + $va12 + $va33" | bc)
 
 echo "average currents:"
 echo "5V line:    $avg_a5 Ampere  (= $va5 VA)"
 echo "12V line:   $avg_a12 Ampere  (= $va12 VA)"
 echo "3.3V line:  $avg_a33 Ampere  (= $va33 VA)"
+echo "-------------"
+echo "= $va_all VA"
 
 exit 0
