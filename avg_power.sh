f7673b34059f07c9e64cbed6c98e8bdeb052e790
Michael Buesch <mb@bu3sch.de>
2009-11-20 17:45:43 +0100

    avg_power: Measure voltages instead of guessing them
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- avg_power.sh
+++ avg_power.sh
@@ -8,7 +8,7 @@
 #
 
 PCIBX="./pcibx -V1 $@"	# path to pcibx
-cnt=70			# number of measurements
+cnt=50			# number of measurements
 
 
 echo "1 + 1" | bc 2>&1 >/dev/null
@@ -31,24 +31,37 @@ S="scale=3;"
 avg_a5="0"
 avg_a12="0"
 avg_a33="0"
+avg_v5="0"
+avg_v12="0"
+avg_v33="0"
 echo -n "measuring" >&2
 for ((i = 0; i < $cnt; i++)); do
 	echo -n "." >&2
 	measure a5
 	avg_a5=$(echo "$S $avg_a5 + $RES" | bc)
+	measure v5uut
+	avg_v5=$(echo "$S $avg_v5 + $RES" | bc)
 	measure a12
 	avg_a12=$(echo "$S $avg_a12 + $RES" | bc)
+	measure v12uut
+	avg_v12=$(echo "$S $avg_v12 + $RES" | bc)
 	measure a33
 	avg_a33=$(echo "$S $avg_a33 + $RES" | bc)
+	measure v33uut
+	avg_v33=$(echo "$S $avg_v33 + $RES" | bc)
 done
 echo "OK" >&2
 echo >&2
 avg_a5=$(echo "$S $avg_a5 / $cnt" | bc)
 avg_a12=$(echo "$S $avg_a12 / $cnt" | bc)
 avg_a33=$(echo "$S $avg_a33 / $cnt" | bc)
-va5=$(echo "$S 5 * $avg_a5" | bc)
-va12=$(echo "$S 12 * $avg_a12" | bc)
-va33=$(echo "$S 3.3 * $avg_a33" | bc)
+avg_v5=$(echo "$S $avg_v5 / $cnt" | bc)
+avg_v12=$(echo "$S $avg_v12 / $cnt" | bc)
+avg_v33=$(echo "$S $avg_v33 / $cnt" | bc)
+
+va5=$(echo "$S $avg_v5 * $avg_a5" | bc)
+va12=$(echo "$S $avg_v12 * $avg_a12" | bc)
+va33=$(echo "$S $avg_v33 * $avg_a33" | bc)
 va_all=$(echo "$S $va5 + $va12 + $va33" | bc)
 
 echo "average currents:"
