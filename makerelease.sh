f6a6d7d30c98bac2e0b8e4eea116bec04b1c0b0a
Michael Buesch <m@bues.ch>
2015-01-05 19:20:05 +0100

    Add makerelease.sh
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ makerelease.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+srcdir="$(dirname "$0")"
+[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
+
+die() { echo "$*"; exit 1; }
+
+# Import the makerelease.lib
+# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+for path in $(echo "$PATH" | tr ':' ' '); do
+	[ -f "$MAKERELEASE_LIB" ] && break
+	MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
+hook_get_version()
+{
+	local file="$1/Makefile"
+	version="$(cat "$file" | grep -e VERSION | head -n1 | cut -d' ' -f3)"
+}
+
+project=pcibx
+makerelease "$@"
